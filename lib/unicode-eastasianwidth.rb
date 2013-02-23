03017643e523a0eb2214fb04198098dd2825de3b
USAMI Kenta <tadsan@zonu.me>
2013-02-24 02:00:57 +0900

    Add eaw_txt.rb

--- lib/unicode-eastasianwidth.rb
+++ lib/unicode-eastasianwidth.rb
@@ -1,7 +1,53 @@
-require "unicode-eastasianwidth/version"
+require 'unicode-eastasianwidth/version'
 
+# Unicode namespace
 module Unicode
+  # EastAsianWidth namespace
   module EastAsianWidth
-    # Your code goes here...
+    #
+    PROPERTY = Struct.new(:symbol, :name, :cjk, :other)
+
+    # East Asian Width property
+    PROPERTY_VALUES = {
+      N:  PROPERTY.new(*%w[N Neutral],   1, 1),
+      A:  PROPERTY.new(*%w[A Ambiguous], 1, 1),
+      H:  PROPERTY.new(*%w[H HalfWidth], 1, 1),
+      W:  PROPERTY.new(*%w[W Wide],      1, 1),
+      F:  PROPERTY.new(*%w[F FullWidth], 1, 1),
+      Na: PROPERTY.new(*%w[Na Narrow],   1, 1),
+    }
+
+    # @return [Hash]
+    def self.versions
+      @@versions ||= {}
+    end
+
+    # @return [Unicode::EastAsianWidth::EawTxt]
+    def self.eaw
+      versions[:DEFAULT]
+    end
+
+    # @param [Symbol,String] version
+    # @return [Unicode::EastAsianWidth::EawTxt]
+    def self.[] (version)
+      key = version.intern
+      eat = versions[key] || EawTxt.new(key)
+      versions[key] ||= eat
+      return eat
+    end
+
+    # @param [Symbol,String] version
+    # @param [Unicode::EastAsianWidth::EawTxt] eawtxt
+    # @return [Unicode::EastAsianWidth::EawTxt]
+    def self.[]= (version, eawtxt)
+      case eawtxt
+      when EawTxt
+        self[version.intern] = eawtxt
+      else
+        raise ArgumentError, "require EawTxt (but #{eawtxt.class})"
+      end
+    end
   end
 end
+
+require 'unicode-eastasianwidth/eaw_txt'
--- lib/unicode-eastasianwidth/eaw_txt.rb
+++ lib/unicode-eastasianwidth/eaw_txt.rb
@@ -1,15 +1,83 @@
-require 'singleton'
-
-# 
+# Each Version EastAsianWidth.txt Class
 class Unicode::EastAsianWidth::EawTxt
-  include Singleton
-
   # EastAsianWidth.txt distribute URI
   URI = 'http://www.unicode.org/Public/UNIDATA/EastAsianWidth.txt'
 
-  # Target EastAsianWidth.txt Version
+  # Target Unicode Version
   KNOWN_VERSION = '6.2.0'
 
-  # Target EastAsianWidth.txt Version Release Date
+  # Target Unicode Version Release Date
   KNOWN_VERSION_RELEASE_AT = Time.gm(2012, 05, 15, 18, 30, 00)
+
+  # Version Check RegExp
+  RE_VERSION = /(\d+)\.(\d+)\.(\d+)/
+
+  types = Unicode::EastAsianWidth::PROPERTY_VALUES.keys.join(?|)
+
+  # Line Parse RegExp
+  RE_LINE_DETAIL = /^(\D+);(#{types}) # (.*)$/
+
+  # Line Parse RegExp
+  RE_LINE_SIMPLE = /^(\D+);(#{types})/
+
+  Line = Struct.new(:unival, :property, :comment)
+
+  attr_reader :version
+
+  # @param [String] ver
+  def initialize (ver)
+    case
+    when version_valid?(ver)
+      @version = ver.intern
+    else
+      raise 'invalid version number.'
+    end
+  end
+
+  # @param [Fixnum] num
+  # @return [Unicode::EastAsianWidth::EawTxt::Line]
+  def [] (num)
+    table[num] ||= nil
+  end
+
+  # @return [Hash]
+  def table
+    @table ||= {}
+  end
+
+  # @param [String,Symbol] ver
+  # @return [Boolean]
+  def version_valid? (ver=nil)
+    ver ||= @version
+    case ver
+    when Symbol
+      version = const_get(ver)
+    else
+      version = ver.to_s
+    end
+    self.class.check_version(version)
+  end
+
+  # @return [Unicode::EastAsianWidth::EawTxt]
+  def self.known_version
+    Unicode::EastAsianWidth[KNOWN_VERSION]
+  end
+
+  # @param [String] version
+  # @return [Boolean]
+  def self.check_version (version)
+    (RE_VERSION =~ version) ? true : false
+  end
+
+  # @param [String] version
+  # @return [Array]
+  def self.split_version_number (version)
+    version.scan(RE_VERSION)[0]
+  end
+
+  # @param [String] line target string
+  # @param [Class] klass
+  def self.line_parse (line, klass=Array)
+    l = line.scan(RE_LINE_SIMPLE)
+  end
 end
